1925ef778e71c69be297280d030578f5f7748bee
Cris-Estrella-Dev <cristian.estrella220@gmail.com>
2024-03-16 09:45:28 -0400

    Module 3 homework done

---
+++ module3-solution/script.js
@@ -0,0 +1,14 @@
+document.getElementById('nav-chicken').addEventListener('click', function() {
+  document.getElementById('food-title').innerText = 'Chicken';
+  document.getElementById('food-description').innerText = 'Chicken, is the best food in the world. It is delicious and nutritious.';
+});
+
+document.getElementById('nav-beef').addEventListener('click', function() {
+  document.getElementById('food-title').innerText = 'Beef';
+  document.getElementById('food-description').innerText = 'Beef, is the best food in the world. It is delicious and nutritious.';
+});
+
+document.getElementById('nav-sushi').addEventListener('click', function() {
+  document.getElementById('food-title').innerText = 'Sushi';
+  document.getElementById('food-description').innerText = 'Sushi, is the best food in the world. It is delicious and nutritious.';
+});
